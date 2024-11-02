7ad1677c8f17b59729107dcb60f310a963d51059
Anay <anaypund123@gmail.com>
2024-11-02 16:42:35 +0530

    Small changes

--- src/routes/main.js
+++ src/routes/main.js
@@ -269,32 +269,11 @@ routes.post('/create-order', async (req, res) => {
     }
   });
 
-routes.post('/verifyOrder',  (req, res)=>{ 
+routes.post('/payment-success',  (req, res)=>{ 
     
-    // STEP 7: Receive Payment Data
-    const {order_id, payment_id} = req.body;     
-    const razorpay_signature =  req.headers['x-razorpay-signature'];
-
-    // Pass yours key_secret here
-    const key_secret = YAEUthsup8SijNs3iveeVlL1;     
-
-    // STEP 8: Verification & Send Response to User
-    
-    // Creating hmac object 
-    let hmac = crypto.createHmac('sha256', key_secret); 
-
-    // Passing the data to be hashed
-    hmac.update(order_id + "|" + payment_id);
-    
-    // Creating the hmac in the required format
-    const generated_signature = hmac.digest('hex');
-    
-    
-    if(razorpay_signature===generated_signature){
-        res.json({success:true, message:"Payment has been verified"})
-    }
-    else
-    res.json({success:false, message:"Payment verification failed"})
+   const {payment_id, order_id, signature, userId} = req.body
+   console.log(payment_id, order_id, signature, userId)
+   res.status(200).redirect('payment-success')
 });
 
 module.exports = routes;
